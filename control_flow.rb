8fc38f8324842ce749f047720e958ac54d27512c
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2022-02-01 14:48:59 -0500

    Fin

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,49 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    puts "Access granted"
+  else
+    puts "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperture < 40
+    puts "It's brisk out there!"
+  elsif temperture >= 40 && temperture <= 65
+    puts "It's a little chilly out there!"
+  elsif temperture > 85
+    puts "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 && num % 5
+    "FizzBuzz"
+  elsif num % 5
+    "Buzz"
+  elsif num % 3
+    "Fizz"
+  else
+    num
+  end
 end
 
+binding.pry
+
 def calculator(operation, num1, num2)
-  # your code here
+  if operation == "+"
+    num1 + num2
+  elsif operation == "-"
+    num1 - num2
+  elsif operation == "*"
+    num1 * num2
+  elsif operation == "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
 end
 
